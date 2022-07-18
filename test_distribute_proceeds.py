4a24bf0814e5891cbe016384624fc1ee23e09a90
Sriram Balakrishnan <sriram.kasilingam@mail.utoronto.ca>
2022-07-18 02:33:35 -0400

    Updated Tests

--- test_distribute_proceeds.py
+++ test_distribute_proceeds.py
@@ -23,44 +23,74 @@ class TestDistributeProceedsForKrakatoaVentures(unittest.TestCase):
         }
     
     def test_distribute_0_dollars(self):
+        cash_proceeds = 0
         class_distribution, member_distribution = distribute_proceeds(
-            0, self.cap_table
-        )
-        
-        self.assertDictEqual(dict(class_distribution), {'A': 0, 'B': 0, 'C': 0})
-        self.assertDictEqual(dict(member_distribution), {'Alex': 0, 'Becky': 0, 'David': 0})
-    
-    def test_distribute_100_dollars(self):
-        class_distribution, member_distribution = distribute_proceeds(
-           10000, self.cap_table
-        )
-        
-        self.assertDictEqual(dict(class_distribution), {'A': 0, 'B': 10000, 'C': 0})
-        self.assertDictEqual(dict(member_distribution), {'Alex': 5000, 'Becky': 5000, 'David': 0})
-    
-    def test_distribute_250_dollars_and_50_cents(self):
-        class_distribution, member_distribution = distribute_proceeds(
-           25050, self.cap_table
-        )
-        
-        self.assertDictEqual(dict(class_distribution), {'A': 0, 'B': 25050, 'C': 0})
-        self.assertDictEqual(dict(member_distribution), {'Alex': 12525, 'Becky': 12525, 'David': 0})
-    
-    def test_distribute_500_dollars(self):
-        class_distribution, member_distribution = distribute_proceeds(
-           50000, self.cap_table
-        )
-        
-        self.assertDictEqual(dict(class_distribution), {'A': 0, 'B': 50000, 'C': 0})
-        self.assertDictEqual(dict(member_distribution), {'Alex': 25000, 'Becky': 25000, 'David': 0})
-    
-    def test_distribute_1000_dollars(self):
-        class_distribution, member_distribution = distribute_proceeds(
-           100000, self.cap_table
+            cash_proceeds, self.cap_table
         )
 
+        class_distribution_total = sum(class_distribution.values())
+        member_distribution_total = sum(member_distribution.values())
+
+        self.assertDictEqual(dict(class_distribution), {'A': 0, 'B': 0, 'C': 0})
+        self.assertAlmostEqual(class_distribution_total, cash_proceeds, delta=1)
+        self.assertDictEqual(dict(member_distribution), {'Alex': 0, 'Becky': 0, 'David': 0})
+        self.assertAlmostEqual(member_distribution_total, cash_proceeds, delta=1)
+    
+    def test_distribute_100_dollars(self):
+        cash_proceeds = 10000
+        class_distribution, member_distribution = distribute_proceeds(
+           cash_proceeds, self.cap_table
+        )
+
+        class_distribution_total = sum(class_distribution.values())
+        member_distribution_total = sum(member_distribution.values())
+
+        self.assertDictEqual(dict(class_distribution), {'A': 0, 'B': 10000, 'C': 0})
+        self.assertAlmostEqual(class_distribution_total, cash_proceeds, delta=1)
+        self.assertDictEqual(dict(member_distribution), {'Alex': 5000, 'Becky': 5000, 'David': 0})
+        self.assertAlmostEqual(member_distribution_total, cash_proceeds, delta=1)
+    
+    def test_distribute_250_dollars_and_50_cents(self):
+        cash_proceeds = 25050
+        class_distribution, member_distribution = distribute_proceeds(
+           cash_proceeds, self.cap_table
+        )
+
+        class_distribution_total = sum(class_distribution.values())
+        member_distribution_total = sum(member_distribution.values())
+
+        self.assertDictEqual(dict(class_distribution), {'A': 0, 'B': 25050, 'C': 0})
+        self.assertAlmostEqual(class_distribution_total, cash_proceeds, delta=1)
+        self.assertDictEqual(dict(member_distribution), {'Alex': 12525, 'Becky': 12525, 'David': 0})
+        self.assertAlmostEqual(member_distribution_total, cash_proceeds, delta=1)
+    
+    def test_distribute_500_dollars(self):
+        cash_proceeds = 50000
+        class_distribution, member_distribution = distribute_proceeds(
+           cash_proceeds, self.cap_table
+        )
+
+        class_distribution_total = sum(class_distribution.values())
+        member_distribution_total = sum(member_distribution.values())
+        
+        self.assertDictEqual(dict(class_distribution), {'A': 0, 'B': 50000, 'C': 0})
+        self.assertAlmostEqual(class_distribution_total, cash_proceeds, delta=1)
+        self.assertDictEqual(dict(member_distribution), {'Alex': 25000, 'Becky': 25000, 'David': 0})
+        self.assertAlmostEqual(member_distribution_total, cash_proceeds, delta=1)
+    
+    def test_distribute_1000_dollars(self):
+        cash_proceeds = 100000
+        class_distribution, member_distribution = distribute_proceeds(
+           cash_proceeds, self.cap_table
+        )
+
+        class_distribution_total = sum(class_distribution.values())
+        member_distribution_total = sum(member_distribution.values())
+        
         self.assertDictEqual(dict(class_distribution), {'A': 14286, 'B': 78572, 'C': 7143})
+        self.assertAlmostEqual(class_distribution_total, cash_proceeds, delta=1)
         self.assertDictEqual(dict(member_distribution), {'Alex': 39286, 'Becky': 46429, 'David': 14286})
+        self.assertAlmostEqual(member_distribution_total, cash_proceeds, delta=1)
         
 
 
